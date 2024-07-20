d5125be736d4227dc7308a16a0cf0768dea5f44a
stakbucks <dhassidu11@gmail.com>
2024-07-21 01:53:53 +0900

    fix: footer address

--- apps/web/src/components/footer/Footer.tsx
+++ apps/web/src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
         <Text typography="t2">보틀즈</Text>
       </div>
       <div className={boxStyle({ margin: 'normal' })}>
-        <Text typography="t1">서울특별시 송파구 잠실동 217-7 301호</Text>
+        <Text typography="t1">서울특별시 송파구 잠실동 217-7 303호</Text>
       </div>
       <div className={boxStyle({ margin: 'normal' })}>
         <Text typography="t1">대표 손인준 | 사업자번호 654-11-02572</Text>
