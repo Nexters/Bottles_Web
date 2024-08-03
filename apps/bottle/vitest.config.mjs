7cc5cc083fe36094402cdabb7de5be625b05fd1f
stakbucks <dhassidu11@gmail.com>
2024-08-03 21:41:55 +0900

    chore(bottle): set path alias for vitest config

--- apps/bottle/vitest.config.mjs
+++ apps/bottle/vitest.config.mjs
@@ -1,9 +1,15 @@
 import baseConfig from '@bottlesteam/vitest-config/vitest.config';
+import { fileURLToPath, URL } from 'url';
 import { mergeConfig, defineConfig } from 'vitest/config';
 
 export default mergeConfig(
   baseConfig,
   defineConfig({
     test: {},
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
   })
 );
