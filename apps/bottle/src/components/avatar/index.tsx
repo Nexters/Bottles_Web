7d15dc1b59a3971019f0be1a4f6f7359474ed2c6
Hwang Taehwan <107744534+stakbucks@users.noreply.github.com>
2024-08-09 21:44:30 +0900

    hotfix(bottle): avatar (#24)
    * fix: filter size prop
    
    * fix(bottle): avatar src

--- apps/bottle/src/components/avatar/index.tsx
+++ apps/bottle/src/components/avatar/index.tsx
@@ -5,7 +5,13 @@ export interface AvatarProps extends Omit<ImageProps, 'alt' | 'width' | 'height'
   size: 'sm' | 'lg';
 }
 
-export function Avatar(props: AvatarProps) {
-  const size = props.size === 'sm' ? 48 : 80;
-  return <Image {...props} alt="user profile image" width={size} height={size} style={{ borderRadius: '50%' }} />;
+export function Avatar({ size: _size, ...props }: AvatarProps) {
+  const size = _size === 'sm' ? 48 : 80;
+  return (
+    <>
+      {props.src != null && (
+        <Image {...props} alt="user profile image" width={size} height={size} style={{ borderRadius: '50%' }} />
+      )}
+    </>
+  );
 }
