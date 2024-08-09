f6687422b937fe02d41999c757da8b2fc96a5d05
stakbucks <dhassidu11@gmail.com>
2024-08-09 21:13:23 +0900

    refactor: token handling logic

--- apps/bottle/src/store/mutation/useAcceptBottleMutation.ts
+++ apps/bottle/src/store/mutation/useAcceptBottleMutation.ts
@@ -1,5 +1,6 @@
 import type { BottleType } from '@/app/bottles/Bottles';
 import { POST, createInit } from '@/features/server';
+import { getClientSideTokens } from '@/features/server/clientSideTokens';
 import { useUserAgent } from '@/features/web-view/UserAgentProvider';
 import { webViewAPI } from '@/features/web-view/api';
 import { Bottle } from '@/models/bottle';
@@ -21,10 +22,11 @@ export function useAcceptBottleMutation<T extends BottleType>(type: T, id: Bottl
     mutationFn: (likeMessage: string | null) =>
       POST<AcceptBottleBody<T>>(
         `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/v1/bottles/${id}/accept`,
+        getClientSideTokens(),
         createInit(getCookie('accessToken') ?? '', { likeMessage })
       ),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: bottlesQueryOptions().queryKey });
+      queryClient.invalidateQueries({ queryKey: bottlesQueryOptions(getClientSideTokens()).queryKey });
       if (type === 'random') {
         webViewAPI({
           type: 'onToastOpen',
