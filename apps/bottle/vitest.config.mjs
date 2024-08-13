d607db95544d68596f71109e0923d14bb2701993
stakbucks <dhassidu11@gmail.com>
2024-08-14 00:30:29 +0900

    fix(bottle): exclude e2e directory for vitest target files

--- apps/bottle/vitest.config.mjs
+++ apps/bottle/vitest.config.mjs
@@ -7,7 +7,16 @@ export default mergeConfig(
   baseConfig,
   defineConfig({
     plugins: [vanillaExtractPlugin()],
-    test: {},
+    test: {
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/e2e/**',
+      ],
+    },
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
