95256e8741e40ea611673400fda2b0a00409601c
stakbucks <dhassidu11@gmail.com>
2024-08-14 23:45:18 +0900

    feat(bottle): add error, not-found page

--- apps/bottle/src/app/error.tsx
+++ apps/bottle/src/app/error.tsx
@@ -1,14 +1,28 @@
 'use client';
 
+import BasketImage from '@/assets/basket.webp';
 import { Header } from '@/components/header';
 import { AppBridgeMessageType, useAppBridge } from '@/features/app-bridge';
-import { Asset } from '@bottlesteam/ui';
+import { Asset, Button, Paragraph, spacings } from '@bottlesteam/ui';
+import Image from 'next/image';
+import { useEffect } from 'react';
+import { errorImageContainer } from './layout.css';
 
-export default function DefaultErrorPage() {
+interface ErrorPageProps {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+export default function DefaultErrorPage({ error, reset }: ErrorPageProps) {
   const { send } = useAppBridge();
 
+  useEffect(() => {
+    // Log error
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
+    <>
       <Header>
         <button
           onClick={() => send({ type: AppBridgeMessageType.WEB_VIEW_CLOSE })}
@@ -17,6 +31,15 @@ export default function DefaultErrorPage() {
           <Asset type="icon-arrow-left" />
         </button>
       </Header>
-    </div>
+      <Paragraph typography="t2" color="black100" style={{ marginTop: spacings.xl, marginBottom: spacings.xxl }}>
+        {'앗, 오류가 발생했어요!\n보틀을 다시 실행해 주세요'}
+      </Paragraph>
+      <div className={errorImageContainer}>
+        <Image alt="basket" src={BasketImage} width={250} height={250} />
+        <Button variant="solid" size="sm" onClick={reset}>
+          다시 시도하기
+        </Button>
+      </div>
+    </>
   );
 }
--- apps/bottle/src/app/not-found.tsx
+++ apps/bottle/src/app/not-found.tsx
@@ -1,22 +1,33 @@
 'use client';
 
+import BasketImage from '@/assets/basket.webp';
 import { Header } from '@/components/header';
 import { AppBridgeMessageType, useAppBridge } from '@/features/app-bridge';
-import { Asset } from '@bottlesteam/ui';
+import { Asset, Button, Paragraph, spacings } from '@bottlesteam/ui';
+import Image from 'next/image';
+import { errorImageContainer } from './layout.css';
 
 export default function DefaultErrorPage() {
   const { send } = useAppBridge();
 
+  const closeWebView = () => send({ type: AppBridgeMessageType.WEB_VIEW_CLOSE });
+
   return (
-    <div>
+    <>
       <Header>
-        <button
-          onClick={() => send({ type: AppBridgeMessageType.WEB_VIEW_CLOSE })}
-          style={{ background: 'none', border: 'none' }}
-        >
+        <button onClick={closeWebView} style={{ background: 'none', border: 'none' }}>
           <Asset type="icon-arrow-left" />
         </button>
       </Header>
-    </div>
+      <Paragraph typography="t2" color="black100" style={{ marginTop: spacings.xl, marginBottom: spacings.xxl }}>
+        {'앗, 오류가 발생했어요!\n존재하지 않는 페이지에요.'}
+      </Paragraph>
+      <div className={errorImageContainer}>
+        <Image alt="basket" src={BasketImage} width={250} height={250} />
+        <Button variant="solid" size="sm" onClick={closeWebView}>
+          돌아가기
+        </Button>
+      </div>
+    </>
   );
 }
