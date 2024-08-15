4940cbd2c6f6ce532e597afcda0bef651a58323c
stakbucks <dhassidu11@gmail.com>
2024-08-16 02:25:15 +0900

    test

--- apps/bottle/src/app/page.tsx
+++ apps/bottle/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <div style={{ width: '100%', height: '200vh', display: 'flex', alignItems: 'flex-end', marginBottom: '300px' }}>
-        <p>폰트 적용확인폰트 적용확인폰트 적용확인폰트</p>
+        <p>폰트 적용확인폰트 적용확인폰트</p>
         <Asset type="icon-down" />
         <Asset type="icon-arrow-left" />
       </div>
