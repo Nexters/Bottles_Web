3c61c2ed3da36c096171dd463727b33c9466a0ca
stakbucks <dhassidu11@gmail.com>
2024-08-15 18:34:38 +0900

    chore(ci): playwright config add browser

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   // forbidOnly: !!process.env.CI,
   // /* Retry on CI only */
