195385833bbe624c4531efb75f6a201fd27022c3
stakbucks <dhassidu11@gmail.com>
2024-08-17 19:52:02 +0900

    parallel run

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   // test commit
   return (
     <div className={layoutStyle}>
-      FINAL TESTssss
+      TEST
       <main className={mainStyle}>
         <Header />
         <Info />
