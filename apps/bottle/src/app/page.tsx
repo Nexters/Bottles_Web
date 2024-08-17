4a924e5a2ed3b9559cbdf3c890a9b5eb09b57fc0
stakbucks <dhassidu11@gmail.com>
2024-08-17 14:47:50 +0900

    parallel run

--- apps/bottle/src/app/page.tsx
+++ apps/bottle/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Asset type="icon-down" />
         <Asset type="icon-arrow-left" />
       </div>
-      <Step.FixedButton style={{ marginBottom: '20px' }}>다음</Step.FixedButton>
+      <Step.FixedButton style={{ marginBottom: '20px' }}>fdsfsds</Step.FixedButton>
     </div>
   );
 }
