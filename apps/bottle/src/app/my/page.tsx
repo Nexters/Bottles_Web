aab15c95d3c84b18d992b3c185dd2be14dac3ebc
stakbucks <dhassidu11@gmail.com>
2024-08-18 19:09:51 +0900

    test:remove suspense

--- apps/bottle/src/app/my/page.tsx
+++ apps/bottle/src/app/my/page.tsx
@@ -1,7 +1,6 @@
 import { getServerSideTokens } from '@/features/server/serverSideTokens';
 import { ServerFetchBoundary } from '@/store/query/ServerFetchBoundary';
 import { myInformationQueryOptions } from '@/store/query/useMyInformation';
-import { Suspense } from 'react';
 import { MyInformation } from './MyInformation';
 
 export default async function MyPage() {
@@ -9,11 +8,9 @@ export default async function MyPage() {
 
   return (
     <>
-      <Suspense>
-        <ServerFetchBoundary fetchOptions={serverFetchOptions}>
-          <MyInformation />
-        </ServerFetchBoundary>
-      </Suspense>
+      <ServerFetchBoundary fetchOptions={serverFetchOptions}>
+        <MyInformation />
+      </ServerFetchBoundary>
     </>
   );
 }
