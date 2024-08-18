548c6863299a988536febedc44118edd48833f3a
stakbucks <dhassidu11@gmail.com>
2024-08-18 19:26:04 +0900

    test:remove suspense

--- apps/bottle/src/app/my/page.tsx
+++ apps/bottle/src/app/my/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSideTokens } from '@/features/server/serverSideTokens';
 import { ServerFetchBoundary } from '@/store/query/ServerFetchBoundary';
 import { myInformationQueryOptions } from '@/store/query/useMyInformation';
+import { Suspense } from 'react';
 import { MyInformation } from './MyInformation';
 
 export default async function MyPage() {
@@ -8,9 +9,11 @@ export default async function MyPage() {
 
   return (
     <>
-      <ServerFetchBoundary fetchOptions={serverFetchOptions}>
-        <MyInformation />
-      </ServerFetchBoundary>
+      <Suspense>
+        <ServerFetchBoundary fetchOptions={serverFetchOptions}>
+          <MyInformation />
+        </ServerFetchBoundary>
+      </Suspense>
     </>
   );
 }
