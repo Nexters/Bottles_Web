ec9cbc0fb7b30a31815c306e8124921a6e61f54b
injoon2019 <ison@sfu.ca>
2024-08-30 17:14:35 +0900

    fix(web): change footer information

--- apps/web/src/components/footer/Footer.tsx
+++ apps/web/src/components/footer/Footer.tsx
@@ -11,10 +11,10 @@ export function Footer() {
         <Text typography="t1">서울특별시 송파구 잠실동 217-7 303호</Text>
       </div>
       <div className={boxStyle({ margin: 'normal' })}>
-        <Text typography="t1">대표 손인준 | 사업자번호 654-11-02572</Text>
+        <Text typography="t1">사업자번호 654-11-02572</Text>
       </div>
       <div className={boxStyle({ margin: 'normal' })}>
-        <Text typography="t1">문의 injun1994@naver.com</Text>
+        <Text typography="t1">문의 bottles.developer@gmail.com</Text>
       </div>
       <div className={boxStyle({ margin: 'normal' })}>
         <Text typography="t1">Copyright © Bottles Corp. All rights reserved</Text>
