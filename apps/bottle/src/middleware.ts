bcd3670098e51bc910ad50c2651978658daae10b
Hwang Taehwan <107744534+stakbucks@users.noreply.github.com>
2024-09-24 22:22:39 +0900

    feat(bottle): handle top notch area (#60)
    * feat(bottle): set device, version info cookie
    
    * fix(bottle): middleware redirect
    
    * feat(bottle): add padding top for notch area

--- apps/bottle/src/middleware.ts
+++ apps/bottle/src/middleware.ts
@@ -3,27 +3,53 @@ import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const url = new URL(request.url);
-  if (String(url).includes('accessToken')) {
-    const accessToken = url.searchParams.get('accessToken') ?? '';
-    const refreshToken = url.searchParams.get('refreshToken') ?? '';
+  const cleanUrl = new URL(url);
 
-    const cleanUrl = new URL(url);
+  let accessToken;
+  let refreshToken;
+  let device;
+  let version;
+
+  if (String(url).includes('accessToken') && String(url).includes('refreshToken')) {
+    accessToken = url.searchParams.get('accessToken') ?? '';
+    refreshToken = url.searchParams.get('refreshToken') ?? '';
     cleanUrl.searchParams.delete('accessToken');
     cleanUrl.searchParams.delete('refreshToken');
-    const response = NextResponse.redirect(cleanUrl.toString());
+  }
 
+  if (String(url).includes('device') && String(url).includes('version')) {
+    device = url.searchParams.get('device')!;
+    version = url.searchParams.get('version')!;
+    cleanUrl.searchParams.delete('device');
+    cleanUrl.searchParams.delete('version');
+  }
+
+  if (url.toString() !== cleanUrl.toString()) {
+    const response = NextResponse.redirect(cleanUrl.toString());
     if (accessToken != null && refreshToken != null) {
       response.cookies.set('accessToken', accessToken, { httpOnly: false });
       response.cookies.set('refreshToken', refreshToken, { httpOnly: false });
+    }
+    if (device != null && version != null) {
+      response.cookies.set('device', device, { httpOnly: false });
+      response.cookies.set('version', version, { httpOnly: false });
     }
 
     return response;
   }
 
   const response = NextResponse.next();
+
   return response;
 }
 
 export const config = {
-  matcher: ['/bottles/:path*', '/my', '/create-profile/:path*', '/profile/create/:path*', '/profile/edit/:path*'],
+  matcher: [
+    '/bottles/:path*',
+    '/my',
+    '/create-profile/:path*',
+    '/profile/create/:path*',
+    '/profile/edit/:path*',
+    '/admin/:path*',
+  ],
 };
