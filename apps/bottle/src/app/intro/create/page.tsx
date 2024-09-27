29eff5a6a9a462e74f6bae4338c8eb37c0e19462
stakbucks <dhassidu11@gmail.com>
2024-09-27 23:51:35 +0900

    feat(bottle): funnel setup

--- apps/bottle/src/app/intro/create/page.tsx
+++ apps/bottle/src/app/intro/create/page.tsx
@@ -1,10 +1,50 @@
-import { getServerSideTokens } from '@/features/server/serverSideTokens';
-import { ServerFetchBoundary } from '@/store/query/ServerFetchBoundary';
-import { currentUserProfileQueryOptions } from '@/store/query/useCurrentUserProfileQuery';
-import type { ReactNode } from 'react';
+'use client';
 
-export default async function CreateIntroLayout({ children }: { children: ReactNode }) {
-  const tokens = getServerSideTokens();
+import { Header } from '@/components/common/header';
+import { Stepper } from '@/components/common/stepper';
+import { Images } from '@/components/intro/images';
+import { Introduction } from '@/components/intro/introduction';
+import { ProfileLayout } from '@/components/profile/layout';
+import { AppBridgeMessageType, useAppBridge } from '@/features/app-bridge';
+import { useFunnel } from '@/features/funnel';
+import { useRouter } from 'next/navigation';
 
-  return <ServerFetchBoundary fetchOptions={currentUserProfileQueryOptions(tokens)}>{children}</ServerFetchBoundary>;
+const MAX_STEPS = 2;
+
+type CreateIntroFunnelValues = {
+  introduction: [{ question: string; answer: string }];
+  // FIXME: depends on server API
+  imageUrl: string;
+};
+
+export default function CreateIntroPage() {
+  const { send } = useAppBridge();
+  const router = useRouter();
+
+  const { onNextStep, currentStep, getValue } = useFunnel<CreateIntroFunnelValues>('/intro/create');
+
+  const steps = [
+    <ProfileLayout key={1}>
+      <Header
+        onGoBack={() => {
+          send({ type: AppBridgeMessageType.WEB_VIEW_CLOSE });
+        }}
+      />
+      <Stepper current={1} max={MAX_STEPS} />
+      <Introduction
+        initialValue={getValue('introduction')}
+        onNext={() => {
+          onNextStep('introduction', [{ answer: '', question: '' }]);
+        }}
+        ctaButtonText="다음"
+      />
+    </ProfileLayout>,
+    <ProfileLayout key={1}>
+      <Header onGoBack={router.back} />
+      <Stepper current={2} max={MAX_STEPS} />
+      <Images initialValue={getValue('imageUrl')} onNext={() => {}} ctaButtonText="완료" />
+    </ProfileLayout>,
+  ];
+
+  return <>{steps[currentStep - 1]}</>;
 }
---
+++ apps/bottle/src/components/intro/images/index.tsx
@@ -0,0 +1,12 @@
+import { ProfileLayout } from '@/components/profile/layout';
+import { BaseProfileComponentProps } from '@/components/profile/types';
+import { CurrentUser } from '@/models/user';
+
+export function Images({ initialValue, onNext, ctaButtonText }: BaseProfileComponentProps<CurrentUser['imageUrl']>) {
+  return (
+    <>
+      <ProfileLayout.Title>{'보틀에 담을 나의\n사진을 골라주세요'}</ProfileLayout.Title>
+      <ProfileLayout.FixedButton onClick={() => {}}>{ctaButtonText}</ProfileLayout.FixedButton>
+    </>
+  );
+}
