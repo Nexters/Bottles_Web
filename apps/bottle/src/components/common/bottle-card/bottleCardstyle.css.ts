2b1bf11545e95a86e7dce60241b92b302a1341b0
Hwang Taehwan <107744534+stakbucks@users.noreply.github.com>
2024-10-09 15:56:22 +0900

    fix(bottle): introduction area width (#74)

--- apps/bottle/src/components/common/bottle-card/bottleCardstyle.css.ts
+++ apps/bottle/src/components/common/bottle-card/bottleCardstyle.css.ts
@@ -19,7 +19,7 @@ export const timeTagStyle = style({
 });
 
 export const introductionBoxStyle = style({
-  width: '296px',
+  width: '100%',
   height: '95px',
   backgroundColor: colors.neutral100,
   padding: spacings.md,
