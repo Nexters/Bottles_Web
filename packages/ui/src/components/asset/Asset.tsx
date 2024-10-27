096a30d6e12a2be5898d6fc0f6d83773ccadd514
stakbucks <dhassidu11@gmail.com>
2024-10-27 15:42:48 +0900

    feat(ui): add icon-close, icon-check white version

--- packages/ui/src/components/asset/Asset.tsx
+++ packages/ui/src/components/asset/Asset.tsx
@@ -2,8 +2,10 @@ import { ComponentProps } from 'react';
 import LeftArrowIcon from './icons/icon_arrow_left.svg';
 import CheckIcon from './icons/icon_check.svg';
 import CheckIconColored from './icons/icon_check_colored.svg';
+import CheckIconWhite from './icons/icon_check_white.svg';
 import ClockIcon from './icons/icon_clock.svg';
 import IconClose from './icons/icon_close.svg';
+import IconCloseWhite from './icons/icon_close_white.svg';
 import DeleteIcon from './icons/icon_delete.svg';
 import DownIcon from './icons/icon_down.svg';
 import PencilIcon from './icons/icon_pencil.svg';
@@ -20,7 +22,9 @@ type Type =
   | 'vector'
   | 'icon-check'
   | 'icon-check-colored'
+  | 'icon-check-white'
   | 'icon-close'
+  | 'icon-close-white'
   | 'icon-delete'
   | 'icon-pencil'
   | 'icon-plus'
@@ -43,8 +47,12 @@ export function Asset({ type, ...rest }: AssetProps) {
     <CheckIcon />
   ) : type === 'icon-check-colored' ? (
     <CheckIconColored />
+  ) : type === 'icon-check-white' ? (
+    <CheckIconWhite />
   ) : type === 'icon-close' ? (
     <IconClose />
+  ) : type === 'icon-close-white' ? (
+    <IconCloseWhite />
   ) : type === 'icon-delete' ? (
     <DeleteIcon />
   ) : type === 'icon-pencil' ? (
