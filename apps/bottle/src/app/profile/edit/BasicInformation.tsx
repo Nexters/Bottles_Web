340f87945b2147dfc897892b6993190403a12b18
stakbucks <dhassidu11@gmail.com>
2024-10-30 21:26:22 +0900

    fix(bottle): block profile image edit on android

--- apps/bottle/src/app/profile/edit/BasicInformation.tsx
+++ apps/bottle/src/app/profile/edit/BasicInformation.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { Profile } from '@/components/common/profile';
+import { useUserAgent } from '@/features/user-agent/UserAgentProvider';
 import { useCurrentUserProfileQuery } from '@/store/query/useCurrentUserProfileQuery';
 import { spacings } from '@bottlesteam/ui';
 import { useRouter } from 'next/navigation';
 
 export function BasicInformation() {
+  const { isAndroid, isMobile } = useUserAgent();
   const {
     data: { imageUrl, age, userName },
   } = useCurrentUserProfileQuery();
@@ -17,9 +19,14 @@ export function BasicInformation() {
       name={userName}
       age={age}
       style={{ marginTop: spacings.xxl }}
-      onEditClick={() => {
-        router.push('/profile/edit/images');
-      }}
+      onEditClick={
+        // TOOD: support Android when feature is ready on native app
+        isAndroid && isMobile
+          ? undefined
+          : () => {
+              router.push('/profile/edit/images');
+            }
+      }
     />
   );
 }
