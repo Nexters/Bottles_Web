af3abbe97d419be115be34e69dd96da12af74267
stakbucks <dhassidu11@gmail.com>
2024-11-02 14:14:38 +0900

    chore(demo): init demo

---
+++ apps/demo/next.config.mjs
@@ -0,0 +1,23 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bottles-bucket.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+  },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+export default withVanillaExtract(nextConfig);
