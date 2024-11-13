cf1f8eca60a5fc7f6ae98b0ecef6a168837b7360
stakbucks <dhassidu11@gmail.com>
2024-11-13 21:58:36 +0900

    feat(bottle): add additional information on GA events

--- apps/bottle/src/app/profile/create/page.tsx
+++ apps/bottle/src/app/profile/create/page.tsx
@@ -39,7 +39,7 @@ export default function CreateProfilePage() {
           <Job
             initialValue={getValue('job')}
             onNext={job => {
-              sendGAEvent('event', 'profile_create_next_click', { value: '1->2' });
+              sendGAEvent('event', 'profile_create_next_click', { value: '1->2', type: '1->2' });
               onNextStep('job', job);
             }}
             ctaButtonText="다음"
@@ -53,7 +53,7 @@ export default function CreateProfilePage() {
           <Height
             initialValue={getValue('height')}
             onNext={height => {
-              sendGAEvent('event', 'profile_create_next_click', { value: '2->3' });
+              sendGAEvent('event', 'profile_create_next_click', { value: '2->3', type: '2->3' });
               onNextStep('height', height);
             }}
             ctaButtonText="다음"
@@ -67,7 +67,7 @@ export default function CreateProfilePage() {
           <Region
             initialValue={getValue('region')}
             onNext={region => {
-              sendGAEvent('event', 'profile_create_next_click', { value: '3->4' });
+              sendGAEvent('event', 'profile_create_next_click', { value: '3->4', type: '3->4' });
               onNextStep('region', region);
             }}
             ctaButtonText="다음"
@@ -81,7 +81,7 @@ export default function CreateProfilePage() {
           <MBTI
             initialValue={getValue('mbti')}
             onNext={mbti => {
-              sendGAEvent('event', 'profile_create_next_click', { value: '4->5' });
+              sendGAEvent('event', 'profile_create_next_click', { value: '4->5', type: '4->5' });
               onNextStep('mbti', mbti);
             }}
             ctaButtonText="다음"
@@ -95,7 +95,7 @@ export default function CreateProfilePage() {
           <Keywords
             initialValue={getValue('keyword')}
             onNext={keyword => {
-              sendGAEvent('event', 'profile_create_next_click', { value: '5->6' });
+              sendGAEvent('event', 'profile_create_next_click', { value: '5->6', type: '5->6' });
               onNextStep('keyword', keyword);
             }}
             ctaButtonText="다음"
@@ -109,7 +109,7 @@ export default function CreateProfilePage() {
           <Interests
             initialValue={getValue('interest')}
             onNext={interest => {
-              sendGAEvent('event', 'profile_create_next_click', { value: '6->7' });
+              sendGAEvent('event', 'profile_create_next_click', { value: '6->7', type: '6->7' });
               onNextStep('interest', interest);
             }}
             ctaButtonText="다음"
@@ -123,9 +123,9 @@ export default function CreateProfilePage() {
           <KakaoId
             initialValue={getValue('kakaoId')}
             onNext={kakaoId => {
-              sendGAEvent('event', 'profile_create_next_click', { value: '6->완료' });
+              sendGAEvent('event', 'profile_create_next_click', { value: '6->완료', type: '6->완료' });
               const duration = getTime();
-              sendGAEvent('event', 'profile_create_duration', { value: duration });
+              sendGAEvent('event', 'profile_create_duration', { value: duration, duration });
               mutate({ ...(getValues() as Profile), kakaoId });
             }}
           />
